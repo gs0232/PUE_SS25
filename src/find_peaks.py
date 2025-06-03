d7133ebb3f57f8f9c56e77fd6aaddfc9adfae5b2
Lilly313 <fl1620@mci4me.at>
2025-06-03 10:22:12 +0200

    funktion ohne aufbaucode

--- src/find_peaks.py
+++ src/find_peaks.py
@@ -1,26 +1,7 @@
 #%%
 import pandas as pd
 
-df_ekg = pd.read_csv("../data/ekg_data/01_Ruhe.txt", sep = "\t", names = ["Voltage in mV", "Time in ms"])
-df_ekg = df_ekg.iloc[0:5000]
 
-treshold = 0.95 * df_ekg["Voltage in mV"].max() 
-min_peak_distance = 10
-
-list_of_index_of_peaks = []
-last_peaks_index = 0
-for index, row in df_ekg.iterrows():
-    if index < df_ekg.index.max() - 1:
-  #wenn row[voltage in mv] größer als  vorherige und nachfolgende ist
-        if row["Voltage in mV"] >= df_ekg.iloc[index - 1]["Voltage in mV"] and row["Voltage in mV"] >= df_ekg.iloc[index + 1]["Voltage in mV"]:
-  #dann füge aktuellen index der liste hinzu
-            if row["Voltage in mV"] > treshold and index - last_peaks_index > min_peak_distance:
-  #wenn treshold überschritten wird und auch der letzte gespeicherte index mindestens den abstand hat
-               list_of_index_of_peaks.append(index)
-               last_peaks_index = index
-print(list_of_index_of_peaks)  
-
-#%%
 def find_peaks(df_ekg, threshold, min_peak_distance):
 
     list_of_index_of_peaks = []
